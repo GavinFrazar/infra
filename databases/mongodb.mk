f93d65295e3ca9beecb2ba68789819368b40e3ec
Gavin Frazar <gavinfrazar@gmail.com>
2023-12-11 14:14:06 -0800

    fix mongodb test command

--- databases/mongodb.mk
+++ databases/mongodb.mk
@@ -7,4 +7,4 @@ $(BUILD)/certs:
 	tctl auth sign --format=mongodb $(SIGN_FLAGS)
 
 $(DB)-tsh-db-connect-flags := --db-user="alice" --db-name="admin" self-hosted-mongodb
-$(DB)-test-input := echo 'select now() from system.local;'
+$(DB)-test-input := echo 'db.runCommand({ ping: 1 })'
