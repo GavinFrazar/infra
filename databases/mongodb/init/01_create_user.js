9522f402709bfa104e9d693d4a94f0a02f5b8ddf
Gavin Frazar <gavinfrazar@gmail.com>
2024-01-19 18:54:53 -0800

    configure mongo for auto user provisioning

--- databases/mongodb/init/01_create_user.js
+++ databases/mongodb/init/01_create_user.js
@@ -5,4 +5,17 @@ db.getSiblingDB("$external").runCommand(
       { role: "readWriteAnyDatabase", db: "admin" }
     ]
   }
-)
+);
+db.getSiblingDB("admin").runCommand({
+    createRole: "teleport-admin-role",
+    privileges: [
+        { resource: { cluster: true }, actions: [ "inprog" ] },
+        { resource: { db: "", collection: "" }, actions: [ "grantRole", "revokeRole" ] },
+        { resource: { db: "$external", "collection": "" }, actions: [ "createUser", "updateUser", "dropUser", "viewUser", "setAuthenticationRestriction", "changeCustomData"] },
+    ],
+    roles: [],
+});
+db.getSiblingDB("$external").runCommand({
+  createUser: "CN=teleport-admin",
+  roles: [ { role: 'teleport-admin-role', db: 'admin' } ],
+});
